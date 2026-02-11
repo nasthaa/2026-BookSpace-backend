a59767b3e141053ce8075d8b5a9b25ae501d0a94
Aini Agusthina <100396317+nasthaa@users.noreply.github.com>
2026-02-11 14:55:50 +0700

    chore: initial project setup

--- obj/Debug/net10.0/BookSpace.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/BookSpace.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookSpace.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d474b717cc7455388c8e7c29ec8394227e8931d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da89c21365eb16e24a98510b3390c2ee56ec33a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookSpace.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookSpace.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
